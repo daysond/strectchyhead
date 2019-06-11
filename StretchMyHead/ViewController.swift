d0773d31b340eaf1f695ef4c7e362e91d52c0df4
Dayson Dong <daysond.yd@gmail.com>
2019-06-11 11:35:50 -0400

    Initial Commit

---
+++ StretchMyHead/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StretchMyHead
+//
+//  Created by Dayson Dong on 2019-06-11.
+//  Copyright © 2019 Dayson Dong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
